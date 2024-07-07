dbd2583932fffbd4717fa5fcbb559708ea486563
Max Gun <max.gun@playstudios-il.com>
2024-07-07 23:40:21 +0300

    catching up with our lessons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StudentForm from './components/StudentForm';
 import StudentList from './components/StudentList';
+import EmailList from './components/EmailList';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-        <div>
-          <Navbar />
-          <StudentForm />
-          <StudentList />
+        <div className='container'>
+          <div class="row">
+            <Navbar />
+          </div>
+          <div class="row">
+            <StudentForm />
+          </div>
+          <div class="row">
+            <div class="col">
+              <StudentList />
+            </div>
+            <div class="col">
+              <EmailList />
+            </div>
+          </div>
         </div>
   );
 }
